fbc911f6e05169340885dc3ed4bdc7b2dc699d99
Degueldre Céline <c.degueldre@student.helmo.be>
2025-09-30 10:59:00 +0200

    switch env api client axios

--- client/src/api/axiosConfig.js
+++ client/src/api/axiosConfig.js
@@ -2,7 +2,8 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    //baseURL: 'http://localhost:5000/api',
+    baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Intercepteur pour ajouter le token JWT à chaque requête
